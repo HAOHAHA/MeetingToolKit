d0afa660ae0a0d2a180da396b894f181b8c5f58a
HAOHAHA <ro8577603@gmail.com>
2016-05-09 02:31:59 +0800

    Revert "Angular replace JQuery"
    This reverts commit bd20d7d89f6f734437ba32eecf23a99fed3ec84a.

--- index.js
+++ index.js
@@ -3,26 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 
-var timmer = {};
-var unlock = true;
-
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/timer.html');
 });
 
 io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
-    console.log(msg);
-    if(msg=="start" && unlock) {
-      unlock = false;
-      myTimer();
-      timmer = setInterval(myTimer, 1000);
-    }else if(msg=="stop") {
-      clearInterval(timmer);
-      io.emit('chat message', msg);
-      socket.broadcast.emit('chat message', msg);
-      unlock = true;
-    }
+      setInterval(myTimer, 1000);
   });
 });
 
@@ -33,7 +20,7 @@ http.listen(3000, function() {
 function myTimer() {
     var d = new Date();
     var startTime = moment().format('YYYY-MM-DD hh:mm:ss');
-    var serverTimeString = moment(startTime);
-    console.log(serverTimeString._d);
+    var serverTimeString = moment(startTime, );
+    console.log(serverTimeString); 
     io.emit('chat message', serverTimeString);
 }
